bf0dd22773e07b173983bfb5ce9264a46bc27831
patAV98 <kh.ff.love98@gmail.com>
2019-02-14 15:54:27 +0100

    Practica hecha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
-  declarations: [
-    AppComponent
+declarations: [
+ AppComponent
+ ],
+imports: [
+  BrowserModule,
+  AppRoutingModule,
+  FormsModule
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+ providers: [],
+ bootstrap: [AppComponent]
+ })
+ export class AppModule { }
